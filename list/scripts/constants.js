bdca30331e3a63d9ad2dccd6f80091b5964c0ce5
KagiJPN <tyu_mom@yahoo.co.jp>
2023-05-24 20:40:03 +0900

    update

---
+++ list/scripts/constants.js
@@ -0,0 +1,7 @@
+const clientId = "h0pe6dkb6r51jzkk27ujasldoqgio9";
+const redirectUri = "https://kagijpn.github.io/twitch-follower-checker/list";
+const scope = "moderator:read:followers";
+
+const accessTokenKey = "twitchAccessToken";
+const previousFollowersKey = "previousFollowersKey";
+const LastCheckedDate = "LastCheckedDate";
