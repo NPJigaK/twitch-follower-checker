d467064b573db11bbbbf54381119ceff3ea1fccd
NPJigaK <tyu_mom@yahoo.co.jp>
2023-10-19 18:45:54 +0900

    delete all

--- list/scripts/constants.js
+++
@@ -1,7 +0,0 @@
-const clientId = "h0pe6dkb6r51jzkk27ujasldoqgio9";
-const redirectUri = "https://twitch-follower-checker.devkey.jp/list/";
-const scope = "moderator:read:followers";
-
-const accessTokenKey = "twitchAccessToken";
-const previousFollowersKey = "previousFollowersKey";
-const lastCheckedDate = "lastCheckedDate";
