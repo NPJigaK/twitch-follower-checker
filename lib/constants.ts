328ad665df6c5c2ef61a257afcde3c25c5b21f4f
NPJigaK <tyu_mom@yahoo.co.jp>
2023-10-21 01:05:19 +0900

    fix redirectUri

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const clientId = "h0pe6dkb6r51jzkk27ujasldoqgio9";
-// const redirectUri = "https://twitch-follower-checker.devkey.jp/list/";
-export const redirectUri = "http://localhost:3000/";
+export const redirectUri = "https://twitch-follower-checker.devkey.jp/";
+// export const redirectUri = "http://localhost:3000/";
 export const scope = "moderator:read:followers";
 export const accessTokenKey = "twitchAccessToken";
 export const lastCheckedDateKey = "lastCheckedDate";
