1a4cdc9e0245a20c9f84526d4d334695cb4d955e
NPJigaK <tyu_mom@yahoo.co.jp>
2023-10-22 20:16:17 +0900

    Add doc for de

--- pages/de/_meta.ts
+++ pages/de/_meta.ts
@@ -2,6 +2,12 @@ const _meta = {
   index: {
     title: "Einleitung",
   },
+  how_to_use: {
+    title: "Anleitung",
+  },
+  contribute: {
+    title: "Mitwirken",
+  },
 };
 
 export default _meta;
